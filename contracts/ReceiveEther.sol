fc577f46dff80749841bfe9a0ceaa11b1dbe3eaa
Saing <saing.sab@gmail.com>
2023-02-21 10:49:18 +0700

    Fallback

---
+++ contracts/ReceiveEther.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract ReceiveEther {
+   /*
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */ 
+   // Function to receive Ether. msg.data must be empty
+   receive() external payable {}
+
+   // Fallback function is called when msg.data is not empty
+   fallback() external payable {}
+
+   function getBalance() public view returns (uint) {
+    return address(this).balance;
+   }
+}
+
+contract SendEther {
+    function sendViaTransfer(address payable _to) public payable {
+        // This function is no longer recommended for sending Ether.
+        _to.transfer(msg.value);
+    }
+
+    function sendVaiCall(address payable _to) public payable {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+
+    }
+}
